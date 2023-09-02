b93f23da26646ac560eb4d2972978ea25e222d7e
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-09-02 11:57:48 +0530

    2 files modified

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -11,15 +11,23 @@ export default function ToDoList() {
   }
 
   const addToTaskArray = ()=>{
-    setTaskArr([...taskArr, taskName])
+
+    setTaskArr([...taskArr, {"task":taskName, "done":false}])
     setTaskName("")
   }
   const deleteFromAray = (index)=>{
     let TempArray=[...taskArr]
     TempArray.splice(index, 1)
     setTaskArr(TempArray)
-
   }
+  const markDone = (index)=>{
+    let TempArray=[...taskArr]
+    TempArray[index]["done"]=true
+    setTaskArr(TempArray)
+    console.log(taskArr)
+  }
+
+
   return (
     <>
     <h1>To Do List</h1>
@@ -27,9 +35,9 @@ export default function ToDoList() {
     <div>
       <h3>Tasks</h3>
       <div class="Tasks">
-      {taskArr.map((task, index)=>{
+      {taskArr.map((taskObj, index)=>{
         return(
-          <div key={index} class="individualTask"><span>{task}</span> <button>Done</button> <button onClick={()=>deleteFromAray(index)}>Delete</button></div>
+          <div key={index} class={taskObj.done? "individualTaskDone":"individualTask"}><span>{index+1}. {taskObj.task}</span> <div><button onClick={()=>markDone(index)}>Done</button> <button onClick={()=>deleteFromAray(index)}>Delete</button></div></div>
         )
       })}
       </div>
